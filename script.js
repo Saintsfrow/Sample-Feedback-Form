43924491b6be139d6b32c9122fa518e0daf02cff
Santhosh <79107018+Saintsfrow@users.noreply.github.com>
2023-02-27 23:51:39 +0530

    Pre-release v0.1

---
+++ script.js
@@ -0,0 +1,87 @@
+var nameError = document.getElementById('name-error');
+var mobileError = document.getElementById('mobile-error');
+var emailError = document.getElementById('email-error');
+var feedbackError = document.getElementById('feedback-error');
+var submitError = document.getElementById('submit-error');
+
+function validateName()
+{
+    var name = document.getElementById('contact-name').value;
+    if(name.length == 0)
+    {
+        nameError.innerHTML = 'Name is required';
+        return false;
+    }
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*&/))
+    {
+        nameError.innerHTML = 'Write full name';
+        return false;
+    }
+    nameError.innerHTML = '<i class="fa-sharp fa-solid fa-check"></i>';
+    return true;
+}
+
+function validateMobile()
+{
+    var mobile = document.getElementById('contact-mobile').value;
+    if(mobile.length == 0)
+    {
+        mobileError.innerHTML = 'Mobile Number is required';
+        return false;
+    }
+    if(mobile.length !== 10)
+    {
+        mobileError.innerHTML = 'Invalid Mobile Number';
+        return false;
+    }
+    if(!mobile.match(/^[0-9]{10}$/))
+    {
+        mobileError.innerHTML = 'Only digits are allowed';
+        return false;
+    }
+    mobileError.innerHTML = '<i class="fa-sharp fa-solid fa-check"></i>';
+    return true;
+}
+
+function validateEmail()
+{
+    var email = document.getElementById('contact-email').value;
+    if(email.length == 0)
+    {
+        emailError.innerHTML = 'Email is required';
+        return false;
+    }
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/))
+    {
+        emailError.innerHTML = 'Invalid Email';
+        return false;
+    }
+    emailError.innerHTML = '<i class="fa-sharp fa-solid fa-check"></i>';
+    return true;
+}
+
+function validateFeedback()
+{
+    var feedback = document.getElementById('contact-feedback').value;
+    var required = 10;
+    var left = required - feedback.length;
+    if(left > 0)
+    {
+        feedbackError.innerHTML = left + ' more characters is required';
+        return false;
+    }
+    feedbackError.innerHTML = '<i class="fa-sharp fa-solid fa-check"></i>';
+    return true;
+}
+
+function validateForm()
+{
+    if(!validateName() || !validateMobile() || !validateEmail() || !validateFeedback())
+    {
+        submitError.style.display = 'block';
+        submitError.innerHTML = 'Fill all the fields required';
+        setTimeout(function(){submitError.style.display = 'none';},3000);
+        return false;
+    }
+
+}
